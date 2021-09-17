1ee04abfba4db197cc67470d088bd0b41df378a8
Jacob Doren <jakedoren@gmail.com>
2021-09-17 16:05:26 -0400

    Wrote all desired test cases

---
+++ tests/Items.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+const app = require('../app')
+
+describe(' Item Crud Operations ', () => {
+
+    it('POST /items -> Should create an item', () => {
+
+    })
+
+    it('GET /items -> Should return all items in the collection', () => {
+
+    })
+
+    it('GET /items:id -> Should get item by ID', () => {
+
+    })
+
+    it('PUT /items:id -> Should update the item by ID', () => {
+
+    })
+
+    it('GET /items:id -> Delete item by id', () => {
+
+    })
+
+})
